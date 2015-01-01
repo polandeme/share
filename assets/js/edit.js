db17c511023c44bcbf2fbb4cdc504821e725edb8
polandeme <polandeme@gmail.com>
2015-01-01 10:28:04 +0800

    testhook

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -18,6 +18,6 @@ if(!($(".post-detail-word").text().trim()) == '' || !($(".post-detail-word")).te
 /*
 $("button").click(function(){
     console.log("test");
-    var url = base_url + 'assets/uploads/images/avatar/ddd.jpg';
+    var  = base_url + 'assets/uploads/images/avatar/ddd.jpg';
     $(".user-msg-basic img").attr('src',url);
 });*/
